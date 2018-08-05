3b07354754fd3a1f8dadfe1ebd40a4ba89f0a9b4
JustinSHong <jhong2009@gmail.com>
2018-08-05 16:52:03 -0700

    set up babel plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,13 @@
+import babel from "rollup-plugin-babel";
+
 export default {
 	entry: "public/js/main.js",
 	dest: "build/js/main.min.js",
 	format: "iife",
-	sourceMap: "inline"
+	sourceMap: "inline",
+	plugins: [
+		babel({
+			exclude: "node_modules/**"
+		})
+	]
 };
