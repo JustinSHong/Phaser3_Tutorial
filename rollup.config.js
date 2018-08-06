843554cbb346023607ee1cc4cede04dc73676aaa
JustinSHong <jhong2009@gmail.com>
2018-08-05 17:09:20 -0700

    set up debug

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import babel from "rollup-plugin-babel";
 import eslint from "rollup-plugin-eslint";
+import resolve from "rollup-plugin-node-resolve";
 
 export default {
 	entry: "public/js/main.js",
@@ -7,6 +8,11 @@ export default {
 	format: "iife",
 	sourceMap: "inline",
 	plugins: [
+		resolve({
+			jsnext: true,
+			main: true,
+			browser: true
+		}),
 		babel({
 			exclude: "node_modules/**"
 		})
