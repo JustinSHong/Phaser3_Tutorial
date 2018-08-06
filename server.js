c4c567a33ffe9860243e6a2cbacafed9e61e02a2
JustinSHong <jhong2009@gmail.com>
2018-08-05 17:58:49 -0700

    set up node/express  server to serve static files

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const helmet = require("helmet");
+const cors = require("cors");
+
+const PORT = 8081;
+
+// middleware
+app.use(helmet());
+app.use(express.json());
+app.use(cors());
+
+// render the static files
+app.use(express.static(__dirname + "/build"));
+app.use(express.static(__dirname + "/public"));
+
+// Root - server serves index.html
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/index.html");
+});
+
+// start server at port
+server.listen(PORT, () => {
+	console.log(`\n==API Running on port ${PORT} ==\n`);
+});
