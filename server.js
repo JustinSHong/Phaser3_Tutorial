72509c5f7efcc1867b193889af61d80a51da0e3e
JustinSHong <jhong2009@gmail.com>
2018-11-24 23:36:24 -0800

    served the assets in /assets

--- server.js
+++ server.js
@@ -12,15 +12,16 @@ app.use(express.json());
 app.use(cors());
 
 // render the static files
+app.use("/assets", express.static(__dirname + "/assets"));
 app.use(express.static(__dirname + "/build"));
 app.use(express.static(__dirname + "/public"));
 
 // Root - server serves index.html
 app.get("/", (req, res) => {
-	res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/index.html");
 });
 
 // start server at port
 server.listen(PORT, () => {
-	console.log(`\n==API Running on port ${PORT} ==\n`);
+    console.log(`\n==API Running on port ${PORT} ==\n`);
 });
