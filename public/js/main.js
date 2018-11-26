82cdb7200e315a5a5ad47a063011cc21d7eb664e
JustinSHong <jhong2009@gmail.com>
2018-11-25 16:50:58 -0800

    created player sprite and set up turning animations for it

--- public/js/main.js
+++ public/js/main.js
@@ -42,6 +42,7 @@ function preload() {
 }
 
 let platforms;
+let player;
 
 // draws game objects in the order in which they are called
 // game objects are added to the Scenes display list
@@ -64,6 +65,36 @@ function create() {
     platforms.create(600, 400, "platform");
     platforms.create(50, 250, "platform");
     platforms.create(750, 220, "platform");
+
+    // creating player sprite - dynamic body
+    player = this.physics.add.sprite(100, 450, "dude");
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    // Animation Manager is a global system - animations created can be shared with all objects
+    // you can define an animation once and apply to many objects
+    // creating animations player sprite can use
+    // 4 frames running left, 4 frames running right, 1 frame facing camera
+    this.anims.create({
+        key: "left",
+        frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1 // loop
+    });
+
+    this.anims.create({
+        key: "turn",
+        frames: [{ key: "dude", frame: 4 }],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: "right",
+        frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
 }
 
 function udpate() {}
